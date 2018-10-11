b277001bf210cc6099822b5229f0ec3535830dd1
ffouillet <felix.fouillet@gmail.com>
2018-10-11 10:49:37 +0200

    Tuned menu element for responsive

---
+++ web/js/snowtrix.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    function enableDesktopMenu() {
+
+        var menu = $('nav:not(#enable-nav)');
+        var menuDesktopId = "snowtrix-menu";
+        var menuMobileId = "menu";
+
+        if($(window).width() > 768) {
+            if($(menu).attr('id') != menuDesktopId) {
+                $('nav#enable-nav').hide(); // Hide link to show default menu
+                $(menu).show();
+                $(menu).appendTo('header'); // Add menu into header
+                $(menu).attr('id',menuDesktopId); // Change menu id so that it does not default menu css rules
+                $(menu).find('.close').hide(); // close hide button.
+            }
+        }
+        else {
+            if($(menu).attr('id') != menuMobileId) {
+                $('nav#enable-nav').show(); // Hide link to show default menu
+                $(menu).appendTo('body'); // Add menu into header
+                $(menu).attr('id',menuMobileId); // Change menu id so that it does not default menu css rules
+                $(menu).find('.close').show(); // close hide button.
+            }
+        }
+    }
+
+    enableDesktopMenu();
+
+    $(window).on('resize', function(){
+        enableDesktopMenu();
+    });
+
+});
