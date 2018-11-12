d8cdbf923cacb52d59384d38c8b09cf575427aab
ffouillet <felix.fouillet@gmail.com>
2018-11-12 21:09:15 +0100

    Corrected a bug that leaves the file created for tests without deleting it in the web dir.

--- tests/CoreBundle/Service/FileUploaderTest.php
+++ tests/CoreBundle/Service/FileUploaderTest.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class FileUploaderTest extends TestCase {
 
     private $fileUploader;
-    private $uploadedFile;
+    private $localFileToUseForTest; // File to simulate the 'file upload' done by end user.
+    private $uploadedFile; // Simulated Symfony's UploadedFile created with localFileToUseForTest
+    private $uploadedFileName; // We need to store it un order to delete it after tests.
     private $targetDirectory;
 
     public function setUp() {
@@ -18,20 +20,20 @@ class FileUploaderTest extends TestCase {
         $this->fileUploader = new FileUploader($this->targetDirectory);
 
         // Create a file in order to test (in the same directory as where the file will be uploaded by the service)
-        $this->localUploadedFile = tempnam($this->targetDirectory, 'uploaded-file-for-test'); // Create the file
+        $this->localFileToUseForTest = tempnam($this->targetDirectory, 'uploaded-file-for-test'); // Create the file
 
-        imagejpeg(imagecreatetruecolor(200,200), $this->localUploadedFile);
+        imagejpeg(imagecreatetruecolor(200,200), $this->localFileToUseForTest);
 
         // Use test mode as the UploadedFile is a local file. Not setting test to true will result in an error.
-        $this->localUploadedFile = new UploadedFile($this->localUploadedFile, 'trick-test-photo', 'image/jpeg', null, null, true);
+        $this->uploadedFile = new UploadedFile($this->localFileToUseForTest, 'trick-test-photo', 'image/jpeg', null, null, true);
 
     }
 
     public function testUploadFile() {
 
-        $uploadedFileName = $this->fileUploader->upload($this->localUploadedFile);
+        $this->uploadedFileName = $this->fileUploader->upload($this->uploadedFile);
 
-        $uploadedFilePath = $this->targetDirectory . $uploadedFileName;
+        $uploadedFilePath = $this->targetDirectory . $this->uploadedFileName;
 
         $this->assertSame(true, file_exists($uploadedFilePath));
     }
@@ -40,13 +42,20 @@ class FileUploaderTest extends TestCase {
 
         $this->expectException('InvalidArgumentException');
 
-        $this->fileUploader->upload($this->localUploadedFile, "");
+        $this->fileUploader->upload($this->uploadedFile, "");
 
     }
 
     public function tearDown() {
-        if(file_exists($this->localUploadedFile)) {
-            unlink($this->localUploadedFile);
+
+        if(file_exists($this->localFileToUseForTest)) {
+            unlink($this->localFileToUseForTest);
+        }
+
+        $uploadedFilePath = $this->targetDirectory . $this->uploadedFileName;
+
+        if(file_exists($uploadedFilePath) && is_file($uploadedFilePath)) {
+            unlink($uploadedFilePath);
         }
     }
 }
